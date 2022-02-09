5fbe48ed5d64fe0314e6d7fc8406679e25e379ac
HUANGHSINWEI <ivanst60914@gmail.com>
2022-02-09 21:24:09 +0800

    07 2/9 21:25

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -6,38 +6,47 @@ using UnityEngine.UI;
 
 public class PlayerInfo : MonoBehaviour
 {
-    //public Image playerHpbar;
-    private int getHit;
+    private static  GameObject playerHpbar;
+    
+    private int[] getHit;
 
-    private List<GameObject> dogMonsters;
-
+    private List<GameObject> dogMonsters; 
     //public Image hpImage;
-    float playerHp;
+    static float playerHp;
     float playerDistance;//人物與怪物的距離
 
 
     void Start()
     {
         dogMonsters = new List<GameObject>();
-        getHit = MonsterDmg.mDogAtk;
-        foreach (var a in GameObject.FindGameObjectsWithTag("Monster"))
-        {
-            dogMonsters.Add(a);
-        }  
-       
+        
+
+        playerHpbar = GameObject.Find("PlayerHpBar");
+        //foreach (var a in GameObject.FindGameObjectsWithTag("Monster"))
+        //{
+        //    dogMonsters.Add(a);
+        //}
+        //getHit = new int[dogMonsters.Count];
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         //for (int a = 0; a < dogMonsters.Count; a++)
         //{
-            MonsterAtack(dogMonsters[1]);
+        //    getHit[a] = MonsterDmg.mDogAtk;
+        //    MonsterAtack(dogMonsters[a],a);
+        //    for (int i = 0; i < 4 ; i++ )
+        //    {
+        //        Debug.Log(i + "    "+ getHit[i]);
+        //    }
         //}
+      // Debug.Log("QQQQQQQQQQQQ" + getHit);
     }
 
-    public void MonsterAtack(GameObject objMonster)
-    {
+    //public void MonsterAtack(GameObject objMonster,int i)
+    //{
         //float a;//算角度分子
 
         //float b;//算角度分母
@@ -47,12 +56,20 @@ public class PlayerInfo : MonoBehaviour
         //a = Vector3.Dot((this.transform.position - objMonster.transform.position), objMonster.transform.forward * 2);
         //b = Vector3.Distance(this.transform.position, objMonster.transform.position) * (objMonster.transform.forward * 2).magnitude;
         //cosValue = a / b;
+       // Debug.Log("...0.0.0......"+cosValue);
 
-        if(Input.GetKeyDown(KeyCode.F))
+        //if (getHit[i] == 1 && cosValue >0.7f)
+        //{           
+        //    playerHpbar.GetComponent<Image>().fillAmount = playerHpbar.GetComponent<Image>().fillAmount- 0.0001f;
+        //}
+        
+    //}
+    public static void PlayerHpCal()
+    {
+        playerHpbar.GetComponent<Image>().fillAmount = playerHpbar.GetComponent<Image>().fillAmount - 0.03f;
+        if(playerHpbar.GetComponent<Image>().fillAmount < 0.3)
         {
-            //getHit == 1
-            //playerHpbar.fillAmount = 0.7f;
-               // playerHpbar.fillAmount - (20.0f / playerHpbar);
+            playerHpbar.GetComponent<Image>().fillAmount = 1;
         }
     }
 }

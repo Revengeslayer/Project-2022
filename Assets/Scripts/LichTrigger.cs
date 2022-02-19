fa7d03e3f7731c547dc0d906f9241a619c91e51d
Revengeslayer <gccbs05591gw2@gmail.com>
2022-02-20 00:00:08 +0800

    09 2/20 0.00

--- Assets/Scripts/LichTrigger.cs
+++ Assets/Scripts/LichTrigger.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class LichTrigger : MonoBehaviour
 {
-
+    private int count;
+    public Animator lich;
     void Update()
     {
-        
+        //Debug.Log("玩家            "+GameObject.Find("Character(Clone)").transform.position);
+        //Debug.Log("觸發            "+ gameObject.transform.position);
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.name== "Character(Clone)")
+        count++;
+        if(other.name== "Character(Clone)"&& count==1)
         {
             Debug.Log("+++++++++++++++++++++++進入");
             Camera.main.depth = -5;
@@ -21,7 +24,19 @@ public class LichTrigger : MonoBehaviour
 
     IEnumerator PlayAnimation()
     {
+        //yield return new WaitUntil(lich.GetCurrentAnimatorStateInfo(0).normalizedTime > 1);
         yield return new WaitForSeconds(2.0f);
         Camera.main.depth = -1;
+    }
+    bool CheckAnimationEnd(int a)
+    {
+        if(a==1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
